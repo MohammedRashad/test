c3522b04dac6d74232a6cfb070416fa2ec02b227
Mohamed Rashad <mrashad74@gmail.com>
2023-03-12 15:25:37 +0400

    test

---
+++ src/components/dashboard/sale_2.js
@@ -0,0 +1,139 @@
+import { Bar } from "react-chartjs-2";
+import { Box, Button, Card, CardContent, CardHeader, Divider, useTheme } from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import ReactPlayer from 'react-player';
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
+// import { Uploader } from "uploader"; // Installed by "react-uploader".
+// import { UploadButton } from "react-uploader";
+
+
+export const Sales2 = (props) => {
+  const theme = useTheme();
+
+  const uploader = Uploader({
+    apiKey: "free" // Get production API keys from Upload.io
+  });
+  
+  // Configuration options: https://upload.io/uploader#customize
+  const options2 = { multi: true };
+  
+  const data = {
+    datasets: [
+      {
+        backgroundColor: "#3F51B5",
+        barPercentage: 0.5,
+        barThickness: 12,
+        borderRadius: 4,
+        categoryPercentage: 0.5,
+        data: [18, 5, 19, 27, 29, 19, 20],
+        label: "This year",
+        maxBarThickness: 10,
+      },
+      {
+        backgroundColor: "#EEEEEE",
+        barPercentage: 0.5,
+        barThickness: 12,
+        borderRadius: 4,
+        categoryPercentage: 0.5,
+        data: [11, 20, 12, 29, 30, 25, 13],
+        label: "Last year",
+        maxBarThickness: 10,
+      },
+    ],
+    labels: ["1 Aug", "2 Aug", "3 Aug", "4 Aug", "5 Aug", "6 Aug", "7 aug"],
+  };
+
+  const options = {
+    animation: false,
+    cornerRadius: 20,
+    layout: { padding: 0 },
+    legend: { display: false },
+    maintainAspectRatio: false,
+    responsive: true,
+    xAxes: [
+      {
+        ticks: {
+          fontColor: theme.palette.text.secondary,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false,
+        },
+      },
+    ],
+    yAxes: [
+      {
+        ticks: {
+          fontColor: theme.palette.text.secondary,
+          beginAtZero: true,
+          min: 0,
+        },
+        gridLines: {
+          borderDash: [2],
+          borderDashOffset: [2],
+          color: theme.palette.divider,
+          drawBorder: false,
+          zeroLineBorderDash: [2],
+          zeroLineBorderDashOffset: [2],
+          zeroLineColor: theme.palette.divider,
+        },
+      },
+    ],
+    tooltips: {
+      backgroundColor: theme.palette.background.paper,
+      bodyFontColor: theme.palette.text.secondary,
+      borderColor: theme.palette.divider,
+      borderWidth: 1,
+      enabled: true,
+      footerFontColor: theme.palette.text.secondary,
+      intersect: false,
+      mode: "index",
+      titleFontColor: theme.palette.text.primary,
+    },
+  };
+
+  return (
+    <Card {...props}>
+      <CardHeader title="Live Stream" />
+      <Divider />
+      <CardContent>
+        <Box
+          sx={{
+            height: 400,
+            position: "relative",
+          }}
+        >
+      
+<img src="/Screen Shot 2023-03-02 at 11.21.42 AM.png" alt="Nature" style={{width: "100%"}} />
+ 
+{/* <UploadButton uploader={uploader}
+                options={options2}
+                onComplete={files => alert(files.map(x => x.fileUrl).join("\n"))}>
+    {({onClick}) =>
+      <button onClick={onClick}>
+        Upload a file...
+      </button>
+    }
+  </UploadButton> */}
+
+
+
+        </Box>
+      </CardContent>
+      <Divider />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          p: 2,
+        }}
+      >
+        <Button color="primary" endIcon={<ArrowRightIcon fontSize="small" />} size="small">
+          Overview
+        </Button>
+      </Box>
+    </Card>
+  );
+};
